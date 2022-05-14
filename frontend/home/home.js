1d9b9275e9aa116b82e4dddbd3f4657666d835ea
zubair1shareef <zubair1shareef@gmail.com>
2022-05-14 19:37:36 +0530

    realtime chat

--- frontend/home/home.js
+++ frontend/home/home.js
@@ -1,15 +1,29 @@
 const addMsgBtn=document.getElementById('addmsgbtn')
 const messageInput=document.getElementById('messageinput')
-
-
-
+var count=0
 document.addEventListener('DOMContentLoaded',async()=>{
+    getMessages()
+})
+
+function callingGetMessagesApi(){
+   setInterval(()=>{
+        getMessages()
+        console.log('dshdf')
+    
+    }
+       
+     , 1000)
+}
+callingGetMessagesApi()
+
+function getMessages(){
     axios.get('http://localhost:3000/getMessage').then(data=>{
         console.log(data.data)
         const dataa=data.data
         let messages_cont_ul=document.getElementById("messages_cont_ul")
-
-        for(i=0;i<dataa.length;i++){
+        
+        
+        for(i=count;i<dataa.length;i++){
             if(i%2==0){
                 messages_cont_ul.innerHTML=messages_cont_ul.innerHTML+ ` <li class="even">${dataa[i].username} : ${dataa[i]. msg} </li>`
         
@@ -21,8 +35,12 @@ document.addEventListener('DOMContentLoaded',async()=>{
             }
         
         }
+        count=dataa.length;
     })
-})
+
+}
+
+
 
 addMsgBtn.addEventListener('click', ()=>{
     const token= localStorage.getItem('token')
